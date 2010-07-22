65eb513abe6b3f03ff0a7625dd95bab8b631381a
mklarmann <mklarmann@googlemail.com>
2010-07-22 18:25:52 +0200

    actual Confirm Dialogs

---
+++ src/ch/eaternity/client/ConfirmDialog.java
@@ -0,0 +1,59 @@
+package ch.eaternity.client;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ConfirmDialog extends DialogBox{
+	interface Binder extends UiBinder<Widget, ConfirmDialog> { }
+	private static final Binder binder = GWT.create(Binder.class);
+
+	@UiField Button closeButton;
+	@UiField Button executeButton;
+	@UiField Label statusLabel;
+
+	public ConfirmDialog(String title) {
+
+		// Use this opportunity to set the dialog's caption.
+		setText(title);
+		setWidget(binder.createAndBindUi(this));
+
+		setAnimationEnabled(true);
+		setGlassEnabled(true);
+	}
+
+
+
+	@Override
+	protected void onPreviewNativeEvent(NativePreviewEvent preview) {
+		super.onPreviewNativeEvent(preview);
+
+		NativeEvent evt = preview.getNativeEvent();
+		if (evt.getType().equals("keydown")) {
+			// Use the popup's key preview hooks to close the dialog when either
+			// enter or escape is pressed.
+			switch (evt.getKeyCode()) {
+			case KeyCodes.KEY_ESCAPE:
+				hide();
+				break;
+			}
+		}
+	}
+
+	@UiHandler("closeButton")
+	void onClicked(ClickEvent event) {
+		hide();
+	}
+
+}
+
