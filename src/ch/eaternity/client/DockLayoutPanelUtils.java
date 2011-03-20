73d1d3bd932199ebc2dfd87600ae230bd041e9ea
mklarmann <mklarmann@googlemail.com>
2011-03-20 19:02:25 +0100

    help panel

---
+++ src/ch/eaternity/client/DockLayoutPanelUtils.java
@@ -0,0 +1,16 @@
+package ch.eaternity.client;
+
+import com.google.gwt.dom.client.Style.Unit; 
+import com.google.gwt.user.client.ui.DockLayoutPanel; 
+import com.google.gwt.user.client.ui.Widget; 
+public class DockLayoutPanelUtils extends DockLayoutPanel { 
+        private DockLayoutPanelUtils() { 
+                super(Unit.PX); 
+        } 
+        public static double getWidgetSize(Widget widget) { 
+                return ((LayoutData) widget.getLayoutData()).size; 
+        } 
+        public void setWidgetSize(Widget widget, double size) { 
+                ((LayoutData) widget.getLayoutData()).size = size; 
+        } 
+} 
