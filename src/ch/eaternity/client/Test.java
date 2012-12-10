9033a42dc274b30b3674be499add4c95bdbcac7e
Aurelian Jaggi <aurelian.jaggi@gmail.com>
2012-12-10 21:46:39 +0100

    Categories Class added

--- src/ch/eaternity/client/Test.java
+++ src/ch/eaternity/client/Test.java
@@ -1,5 +1,107 @@
 package ch.eaternity.client;
 
-public class Test {
+import java.util.List;
+import java.util.Map;
 
+import ch.eaternity.shared.Ingredient;
+import ch.eaternity.shared.IngredientSpecification;
+import ch.eaternity.shared.Recipe;
+
+
+public class Test {
+// Input: List<recipe>
+	
+	Map<String,Ingredient> getIngsByCategory(List<IngredientSpecification> ingredients, String category)
+	{
+		
+	}
+}
+
+getIngsByCategory(String category, List<IngredientSpecification> ingredients)
+getIngsByDate()
+
+List<IngredientSpecification> getIngsFromRecipes(List<Recipe> recipes) {}
+
+Long calculateTotalC02Value(List<IngredientSpecification> ingredients) {}
+
+
+getIngredientsByFormula() {}
+
+Ingredient getIngFromId(int id)
+
+//Example For Loops in report.jsp
+for (Date date : dates)
+{	
+	for ()
+}
+
+
+// -------------------------------------
+Liste:
+(("<strong>Vegetable Products</strong>"),("meat", "animal-based")),
+Rice products
+Spices & herbs
+Sweets
+Vegetable oils and fat
+Vegetables and fruits
+Preprocessed vegetable products
+Bread and Grain Products -> grain + bread + pasta
+Nuts und seeds
+
+<strong><strong>Animal Products</strong></strong>
+Non-ruminants
+Ruminants
+Fish and seafood
+
+Animal based fats -> oil and fats + animal-based
+Ripened cheese
+Fresh cheese and diary products
+
+Eggs and egg based products
+Canned and finished products ->
+Sauces
+
+<strong>Drinks</strong>
+Drinks (alkohol based)
+Drinks (fruit based)
+Drinks (milk based)
+
+Liste:
+	Dates
+
+List<Ingredient> ingredients = new ArrayList<Ingredient>();
+ingredients = dao.getAllIngredients()
+
+for(Ingredients ingredient: ingredients){
+	ingredient.tags
+}
+
+
+// add
+List<Ingredients> getIngredientsByTagAdded(List<Ingredients> ingredients, List<Tag> tags) {
+	List<Ingredients> filteredList = new ArrayList<Ingredients>();
+	
+	for(Tag tag: tags) {
+		filteredList.append(getIngredientsByTag(ingredients, tag));
+	}
+	
+}
+
+// subtract
+
+List<Ingredients> getIngredientsByTagSubtract(List<Ingredients> ingredients, List<Tag> tags) {
+	List<Ingredients> filteredList = new ArrayList<Ingredients>();
+	
+	for(Tag tag: tags) {
+		filteredList.removeAll(getIngredientsByTag(ingredients, tag));
+	}
+	
+}
+
+filteredList.removeDuplicates();
+
+
+// filter
+List<Ingredients> getIngredientsByTag(List<Ingredients>, Tag tag){
+	
 }
---
+++ src/ch/eaternity/shared/Categories.java
@@ -0,0 +1,35 @@
+package ch.eaternity.shared;
+
+import java.util.List;
+import java.util.Date;
+
+
+public class Categories {
+	public class CategoryValue {
+		public String categoryName;
+		public Long co2value;
+		
+		public CategoryValue(){
+		}
+		
+		public CategoryValue(String name, Long value) {
+			this.categoryName = name;
+			this.co2value = value;
+		}
+	}
+	
+	public class CategoryValuesByDates {
+		List<CategoryValue> category;
+		List<Date> date;
+		
+		public CategoryValuesByDates(){
+		}
+		
+		public CategoryValuesByDates(List<CategoryValue> category, List<Date> date){
+			this.category = category;
+			this.date = date;
+		}
+		
+	}
+
+}
