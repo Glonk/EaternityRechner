144f04964448644030f664f865d4dfd4965698c5
Aurelian Jaggi <ajaggi@eaternity.ch>
2013-05-06 18:44:35 +0200

    custom bootstrap resourc added

---
+++ src/ch/eaternity/client/resources/BootstrapConfigurator.java
@@ -0,0 +1,15 @@
+package ch.eaternity.client.resources;
+
+import com.github.gwtbootstrap.client.ui.config.Configurator;
+import com.github.gwtbootstrap.client.ui.resources.Resources;
+import com.google.gwt.core.client.GWT;
+
+public class BootstrapConfigurator implements Configurator {
+	public Resources getResources() {
+		return GWT.create(BootstrapResources.class);
+	}
+	
+	public boolean hasResponsiveDesign() {
+		return true;
+	}
+}
---
+++ src/ch/eaternity/client/resources/BootstrapResources.java
@@ -0,0 +1,13 @@
+package ch.eaternity.client.resources;
+
+import com.google.gwt.resources.client.TextResource;
+import com.github.gwtbootstrap.client.ui.resources.Resources;
+
+public interface BootstrapResources extends Resources {
+	/*
+    @Source("bootstrap.min.css")
+    TextResource bootstrapCss();*/
+
+    @Source("bootstrap-responsive.min.css")
+    TextResource bootstrapResponsiveCss();
+}
