282fafcc5131d2c5d68bdba80d00d78a870b683d
Aurelian Jaggi <ajaggi@eaternity.ch>
2013-05-16 17:12:13 +0200

    feedback link

--- src/ch/eaternity/client/ui/SearchIngredients.java
+++ src/ch/eaternity/client/ui/SearchIngredients.java
@@ -218,7 +218,7 @@ public class SearchIngredients extends ResizeComposite {
 	// Feedback
 	@UiHandler("feedbackAnchor")
 	public void onFeedbackClick(ClickEvent event) {
-		feedbackModal.show();
+		//feedbackModal.show();
 	}
 	@UiHandler("closeButton")
 	public void onCloseClick(ClickEvent event) {
